7725500014d4cba36f6b62f2d1a072c672a9cb4c
basavaraj <basavcoder@gmail.com>
2024-05-07 11:49:13 +0530

    second commit changes in logic enum added optimised the code

--- lib/Calculator/calculator_screen.dart
+++ lib/Calculator/calculator_screen.dart
@@ -1,4 +1,5 @@
 //import 'package:calculator/Btn/Btn_cal.dart';
+import 'package:calculator/Calculator/calculator_field_enum.dart';
 import 'package:calculator/FormField/TFromField.dart';
 import 'package:flutter/material.dart';
 
@@ -20,6 +21,7 @@ class CalculatorState extends State<Calculator> {
   TextEditingController number1Controler = TextEditingController();
   TextEditingController number2Controler = TextEditingController();
   String res = "0";
+  double sum=0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -61,90 +63,36 @@ class CalculatorState extends State<Calculator> {
                 children: [
                   FilledButton(
                       onPressed: () {
-                        if (_formKey.currentState != null &&
-                            _formKey.currentState!.validate()) {
-                          //above line calls validate()
-                          // print("values from field 1 ${number1Controler.text}");
-                          // print("values from field 2 ${number2Controler.text}");
-                          // print("added value ${number1Controler.text+number2Controler.text}");
-                          int temp1 = int.parse(number1Controler.text);
-                          int temp2 = int.parse(number2Controler.text);
-                          int sum=  temp1+temp2;
-                          res = sum.toString();
-                          clearText();
-                        }
+                        calculate(CalculatorFieldEnum.ADDITION_BUTTON);
                       },
-                      child: const Text("Add",style: TextStyle(fontSize: 20),)),
-                  // Btn_cal(
-                  //     formKey: _formKey,
-                  //     number1Controller: number1Controler,
-                  //     number2Controller: number2Controler,
-                  //     displayTextbtn: "add"),
-                  // Btn_cal(
-                  //     formKey: _formKey,
-                  //     number1Controller: number1Controler,
-                  //     number2Controller: number2Controler,
-                  //     displayTextbtn: "sub"),
-                  // Btn_cal(
-                  //     formKey: _formKey,
-                  //     number1Controller: number1Controler,
-                  //     number2Controller: number2Controler,
-                  //     displayTextbtn: "mul"),
-                  // Btn_cal(
-                  //     formKey: _formKey,
-                  //     number1Controller: number1Controler,
-                  //     number2Controller: number2Controler,
-                  //     displayTextbtn: "div"),
-
+                      child: const Text(
+                        "Add",
+                        style: TextStyle(fontSize: 20),
+                      )),
                   FilledButton(
                       onPressed: () {
-                        if (_formKey.currentState != null &&
-                            _formKey.currentState!.validate()) {
-                          //above line calls validate()
-                          // print("values from field 1 ${number1Controler.text}");
-                          // print("values from field 2 ${number2Controler.text}");
-                          int temp1 = int.parse(number1Controler.text);
-                          int temp2 = int.parse(number2Controler.text);
-                          int sum = temp1-temp2;
-                          res = sum.toString();
-                          clearText();
-                         // print("sub values $res");
-                        }
+                        calculate(CalculatorFieldEnum.SUBSTRACTION_BUTTON);
                       },
-                      child: const Text("Sub",style: TextStyle(fontSize: 20),)),
+                      child: const Text(
+                        "Sub",
+                        style: TextStyle(fontSize: 20),
+                      )),
                   FilledButton(
                       onPressed: () {
-                        if (_formKey.currentState != null &&
-                            _formKey.currentState!.validate()) {
-                          //above line calls validate()
-                          // print("values from field 1 ${number1Controler.text}");
-                          // print("values from field 2 ${number2Controler.text}");
-                          int temp1 = int.parse(number1Controler.text);
-                          int temp2 = int.parse(number2Controler.text);
-                          int sum = temp1*temp2;
-                          res = sum.toString();
-                          clearText();
-
-                        }
+                        calculate(CalculatorFieldEnum.MULTIPLICATION_BUTTON);
                       },
-                      child: const Text("Mul",style: TextStyle(fontSize: 20),)),
+                      child: const Text(
+                        "Mul",
+                        style: TextStyle(fontSize: 20),
+                      )),
                   FilledButton(
                       onPressed: () {
-                        if (_formKey.currentState != null &&
-                            _formKey.currentState!.validate()) {
-                          //above line calls validate()
-                          print("values from field 1 ${number1Controler.text}");
-                          print("values from field 2 ${number2Controler.text}");
-
-                          int temp1 = int.parse(number1Controler.text);
-                          int temp2 = int.parse(number2Controler.text);
-                          double sum = temp1/temp2;
-                          res = sum.toString();
-                          clearText();
-
-                        }
+                        calculate(CalculatorFieldEnum.DIVISION_BUTTON);
                       },
-                      child: const Text("Div",style: TextStyle(fontSize: 20),))
+                      child: const Text(
+                        "Div",
+                        style: TextStyle(fontSize: 20),
+                      ))
                 ],
               )
             ],
@@ -155,10 +103,53 @@ class CalculatorState extends State<Calculator> {
   }
 
   void clearText() {
-    setState(() {
-
-    });
+    setState(() {});
     number1Controler.text = "";
     number2Controler.text = "";
+  }
+
+  void calculate(CalculatorFieldEnum cenum) {
+
+    if (_formKey.currentState != null && _formKey.currentState!.validate()) {
+      //above line calls validate()
+      print("values from field 1 ${number1Controler.text}");
+      print("values from field 2 ${number2Controler.text}");
+      int temp1 = int.parse(number1Controler.text);
+      int temp2 = int.parse(number2Controler.text);
+
+      switch (cenum) {
+        case CalculatorFieldEnum.ADDITION_BUTTON:
+          {
+            sum = (temp1 + temp2) as double;
+            print("hashcode ${sum.hashCode}");
+            res = sum.toString();
+            clearText();
+          }
+          break;
+        case CalculatorFieldEnum.SUBSTRACTION_BUTTON:
+          {
+            sum = (temp1 - temp2) as double;
+            print("hashcode ${sum.hashCode}");
+            res = sum.toString();
+            clearText();
+          }
+          break;
+        case CalculatorFieldEnum.MULTIPLICATION_BUTTON:
+          {
+            sum = (temp1 * temp2) as double;
+            print("hashcode ${sum.hashCode}");
+            res = sum.toString();
+            clearText();
+          }
+          break;
+        case CalculatorFieldEnum.DIVISION_BUTTON:
+          {
+            sum = temp1 / temp2;
+            res = sum.toString();
+            clearText();
+          }
+          break;
+      }
+    }
   }
 }
